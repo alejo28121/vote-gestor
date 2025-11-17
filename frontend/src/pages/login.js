063b2b8acf4f6aa762864822de9d2ca726324499
Alejandro Grajales Vargas <grajalesvargasalejandro@gmail.com>
2025-11-16 19:23:05 -0500

    Beggining of login

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,14 @@
+import '../assets/styles/login.css'
+
+function Login (){
+    return(
+        <div className="Main-login-container">
+            <div className='Form-login-container'>
+                <form>
+                    
+                </form>
+            </div>
+        </div>
+    );
+}
+export default Login;
