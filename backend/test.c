9396c366a198cc3ae4a4679c28dc31af11184bd6
axelmauriciobolanosmartinez-pixel <axelmauriciobolanosmartinez@gmail.com>
2025-11-17 17:18:25 -0500

    Funcion Sistem add

--- backend/test.c
+++ backend/test.c
@@ -3,6 +3,45 @@
 #include <string.h>
 #include "cJSON.h"
 
+void ValidateUser() {
+    FILE *f;
+    char usuario[50], password[50];
+    char u[50], p[50];
+    int encontrado = 0;
+
+    // Si el archivo no existe, lo crea automáticamente 
+    f = fopen("usuarios.txt", "a+");
+    if (!f) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    // Pedir datos al usuario 
+    printf("Ingrese usuario: ");
+    scanf("%s", usuario);
+
+    printf("Ingrese contrasena: ");
+    scanf("%s", password);
+
+    // Regresar al inicio del archivo
+    rewind(f);
+
+    // Buscar usuario y contraseña
+    while (fscanf(f, "%s %s", u, p) == 2) {
+        if (strcmp(usuario, u) == 0 && strcmp(password, p) == 0) {
+            encontrado = 1;
+            break;
+        }
+    }
+
+    if (encontrado == 1)
+        printf("OK\n");
+    else
+        printf("Invalido\n");
+
+    fclose(f);
+}
+
 int main() {
     char buffer[4096];
     size_t n = fread(buffer, 1, sizeof(buffer) - 1, stdin); // dejar espacio para \0
