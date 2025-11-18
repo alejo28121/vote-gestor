9f02709c2ee7c805d46f0ac67ad7badaf698376c
Alejandro Grajales Vargas <grajalesvargasalejandro@gmail.com>
2025-11-18 17:51:51 -0500

    Add prototype for login

--- backend/test.c
+++ backend/test.c
@@ -5,40 +5,71 @@
 
 void ValidateUser() {
     FILE *f;
-    char usuario[50], password[50];
     char u[50], p[50];
     int encontrado = 0;
+    char buffer[4096];
+    size_t n = fread(buffer, 1, sizeof(buffer) - 1, stdin); 
+    buffer[n] = '\0'; 
+    cJSON *root = cJSON_Parse(buffer);
+    if (!root) {
+        fprintf(stderr, "Invalid JSON\n");
+        return;
+    }
+    cJSON *userItem = cJSON_GetObjectItem(root, "user");
+    if (!userItem || !cJSON_IsString(userItem)) {
+        fprintf(stderr, "Missing or invalid 'user'\n");
+        cJSON_Delete(root);
+        return;
+    }
+    char *user = userItem->valuestring;
+
+    cJSON *passwordItem = cJSON_GetObjectItem(root, "password");
+    if (!passwordItem || !cJSON_IsString(passwordItem)) {
+        fprintf(stderr, "Missing or invalid 'user'\n");
+        cJSON_Delete(root);
+        return;
+    }
+    char *password = passwordItem->valuestring;
 
     // Si el archivo no existe, lo crea automáticamente 
-    f = fopen("usuarios.txt", "a+");
+    f = fopen("C:\\Users\\graja\\OneDrive\\Documentos\\Proyectos\\voteManager\\backend\\usuarios.txt", "a+");
     if (!f) {
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-
-    // Pedir datos al usuario 
-    printf("Ingrese usuario: ");
-    scanf("%s", usuario);
-
-    printf("Ingrese contrasena: ");
-    scanf("%s", password);
-
-    // Regresar al inicio del archivo
     rewind(f);
-
     // Buscar usuario y contraseña
     while (fscanf(f, "%s %s", u, p) == 2) {
-        if (strcmp(usuario, u) == 0 && strcmp(password, p) == 0) {
+        if (strcmp(user, u) == 0 && strcmp(password, p) == 0) {
             encontrado = 1;
             break;
         }
     }
 
-    if (encontrado == 1)
-        printf("OK\n");
-    else
-        printf("Invalido\n");
-
+    if (encontrado == 1){
+        cJSON *resp = cJSON_CreateObject();
+        cJSON_AddStringToObject(resp, "status", "ok");
+        cJSON_AddStringToObject(resp, "mensaje", "User was logined");
+        char *json = cJSON_Print(resp);
+        if (json) {
+            printf("%s", json);
+            cJSON_free(json);
+        }
+        cJSON_Delete(resp);
+        cJSON_Delete(root);
+    }
+    else{
+        cJSON *resp = cJSON_CreateObject();
+        cJSON_AddStringToObject(resp, "status", "Invalid");
+        cJSON_AddStringToObject(resp, "mensaje", "Incorrect user or password");
+        char *json = cJSON_Print(resp);
+        if (json) {
+            printf("%s", json);
+            cJSON_free(json);
+        }
+        cJSON_Delete(resp);
+        cJSON_Delete(root);
+    }
     fclose(f);
 }
 
@@ -50,7 +81,7 @@ void RegisVotes() {
     int candidato;
 
     // Abrir o crear Votes.txt 
-    f = fopen("Votes.txt", "a");
+    f = fopen("C:\\Users\\graja\\OneDrive\\Documentos\\Proyectos\\voteManager\\backend\\Votes.txt", "a");
     if (!f) {
         printf("Error al crear o abrir Votes.txt\n");
         return;
@@ -70,34 +101,10 @@ void RegisVotes() {
     printf("Voto registrado correctamente.\n");
 
     fclose(f);
+
 }
 
 int main() {
-    char buffer[4096];
-    size_t n = fread(buffer, 1, sizeof(buffer) - 1, stdin); 
-    buffer[n] = '\0'; 
-    cJSON *root = cJSON_Parse(buffer);
-    if (!root) {
-        fprintf(stderr, "Invalid JSON\n");
-        return 1;
-    }
-    cJSON *userItem = cJSON_GetObjectItem(root, "user");
-    if (!userItem || !cJSON_IsString(userItem)) {
-        fprintf(stderr, "Missing or invalid 'user'\n");
-        cJSON_Delete(root);
-        return 1;
-    }
-    char *user = userItem->valuestring;
-    cJSON *resp = cJSON_CreateObject();
-    cJSON_AddStringToObject(resp, "status", "ok");
-    cJSON_AddStringToObject(resp, "mensaje", "User was registred");
-    char *json = cJSON_Print(resp);
-    if (json) {
-        printf("%s", json);
-        cJSON_free(json);
-    }
-    cJSON_Delete(resp);
-    cJSON_Delete(root);
     ValidateUser();
     return 0;
 }
