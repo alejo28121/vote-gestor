661363e0ad67adb27e0887a308e2b9f3e8b58293
alejo28121 <grajalesvargasalejandro@gmail.com>
2025-11-24 18:21:46 -0500

    Add delete and add candidates

--- frontend/src/components/editcandidates.js
+++ frontend/src/components/editcandidates.js
@@ -1,15 +1,19 @@
 import '../assets/styles/candidates.css'
 import DropIcon from '../assets/icons/drop.svg'
+import Edit from '../assets/icons/edit.svg'
+import Delete from '../assets/icons/delete.svg'
+import Add from '../assets/icons/add.svg'
 import { useState } from 'react';
 
 function Candidates({candidates}) {
     const [listValue, setListValue] = useState(0);
+    const [edit, setEdit] = useState(false);
     return (
         <div className="list-votes">
             <h2 className="Vote-title">Candidatos</h2>
             <div className="Container-candidates">
                 <div className="Item">
-                    <div className="Title-item-container">
+                    <div className={`Title-item-container${listValue === 1 ? '-s' : ''}`}>
                         <h3 className="Title-item">Candidatos Presidenciales</h3>
                         <img className='Drop-icon' src={DropIcon} onClick={() =>
                             setListValue(1)
@@ -17,32 +21,85 @@ function Candidates({candidates}) {
                     </div>
                     <div className={`Items-container-list${listValue === 1 ? '-1' : ''}`}>
                         {Object.entries(candidates).map(([candidate]) => (
-                            <p className='Candidate-name' key={candidate}>
-                                {candidate}
-                            </p>
+                            <div className='Item-check'>
+                                <p className='Candidate-name' key={candidate}>
+                                    {candidate}
+                                </p>
+                                <input type='checkbox' className={`check${edit ? '' : '-o'}`}></input>
+                            </div>
                         ))}
+                        <div className='Buttons-container'>
+                            <img className={`Edit-icon${edit ? '-c' : ''}`} src={Edit} onClick={() => 
+                                setEdit(true)
+                            }></img>
+                            <img className={`Delete-icon${edit ? '-c' : ''}`} src={Delete}></img>
+                            <img className={`Add-icon${edit ? '-c' : ''}`} src={Add}></img>                            
+                        </div>
+                        <div className={`Buttons-sucess${edit ? '-c' : ''}`}>
+                            <button className='Save-button' onClick={() => 
+                                setEdit(false)
+                            }>Guardar</button>
+                        </div>
                     </div>
                 </div>
                 <div className="Item">
-                    <div className="Title-item-container">
+                    <div className={`Title-item-container${listValue === 2 ? '-s' : ''}`}>
                         <h3 className="Title-item">Candidatos a Camara</h3>
                         <img className='Drop-icon' src={DropIcon} onClick={() =>
                             setListValue(2)
                         }></img>
                     </div>
                     <div className={`Items-container-list${listValue === 2? '-2' : ''}`}>
-                        <p>Hola como estas como te llamas</p>
+                        {Object.entries(candidates).map(([candidate]) => (
+                            <div className='Item-check'>
+                                <p className='Candidate-name' key={candidate}>
+                                    {candidate}
+                                </p>
+                                <input type='checkbox' className={`check${edit ? '' : '-o'}`}></input>
+                            </div>
+                        ))}
+                        <div className='Buttons-container'>
+                            <img className={`Edit-icon${edit ? '-c' : ''}`} src={Edit} onClick={() => 
+                                setEdit(true)
+                            }></img>
+                            <img className={`Delete-icon${edit ? '-c' : ''}`} src={Delete}></img>
+                            <img className={`Add-icon${edit ? '-c' : ''}`} src={Add}></img>                            
+                        </div>
+                        <div className={`Buttons-sucess${edit ? '-c' : ''}`}>
+                            <button className='Save-button' onClick={() => 
+                                setEdit(false)
+                            }>Guardar</button>
+                        </div>
                     </div>
                 </div>
                 <div className="Item">
-                    <div className="Title-item-container">
+                    <div className={`Title-item-container${listValue === 3 ? '-s' : ''}`}>
                         <h3 className="Title-item">Candidatos a Concejo</h3>
                         <img className='Drop-icon' src={DropIcon} onClick={() =>
                             setListValue(3)
                         }></img>
                     </div>
                     <div className={`Items-container-list${listValue === 3? '-3' : ''}`}>
-                        <p>Hola como estas como te llamas</p>
+                        {Object.entries(candidates).map(([candidate]) => (
+                            <div className='Item-check'>
+                                <p className='Candidate-name' key={candidate}>
+                                    {candidate}
+                                </p>
+                                <input type='checkbox' className={`check${edit ? '' : '-o'}`}></input>
+                            </div>
+                        ))}
+                        <div className='Buttons-container'>
+                            <img className={`Edit-icon${edit ? '-c' : ''}`} src={Edit} onClick={() => 
+                                setEdit(true)
+                            }></img>
+                            <img className={`Delete-icon${edit ? '-c' : ''}`} src={Delete}></img>
+                            <img className={`Add-icon${edit ? '-c' : ''}`} src={Add}></img>                            
+                        </div>
+                        <div className={`Buttons-sucess${edit ? '-c' : ''}`}>
+                            <button className='Save-button' onClick={() => 
+                                setEdit(false)
+                            }>Guardar</button>
+                        </div>
                     </div>
                 </div>
             </div>
